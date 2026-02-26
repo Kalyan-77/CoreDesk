80dfc64a5055a868322ca34b4152cef1f251692d
kalyan <your-email@example.com>
2026-02-26 14:24:38 +0530

    Folder Structure

---
+++ Backend/src/main/java/com/kalyan/CoreDesk/CoreDeskApplication.java
@@ -0,0 +1,13 @@
+package com.kalyan.CoreDesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreDeskApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/kalyan/CoreDesk/CoreDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalyan.CoreDesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
